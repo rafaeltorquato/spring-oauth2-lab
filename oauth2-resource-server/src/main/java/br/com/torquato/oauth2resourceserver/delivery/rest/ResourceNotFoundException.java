b0134d485dc4c72fd358a7282e43bab9e66b591f
Rafael Torquato <rafael.torquato@ebserh.gov.br>
2021-10-23 08:40:38 -0300

    Remove unused imports

--- oauth2-resource-server/src/main/java/br/com/torquato/oauth2resourceserver/delivery/rest/ResourceNotFoundException.java
+++ oauth2-resource-server/src/main/java/br/com/torquato/oauth2resourceserver/delivery/rest/ResourceNotFoundException.java
@@ -1,8 +1,4 @@
 package br.com.torquato.oauth2resourceserver.delivery.rest;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 }
